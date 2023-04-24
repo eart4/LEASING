657646f0b1e2e0d56a74f8923f8202ab0907871f
eart4 <metrika987@gmail.com>
2023-04-24 21:31:48 +0300

    add: added effect fade

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const swiper = new Swiper(".swiper", {
+  effect: "fade",
     autoplay: {
       delay: 3000,
       disableOnInteraction: false,
+
     },
     pagination: {
       el: ".swiper-pagination",
