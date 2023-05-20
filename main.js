5a1277f81c544843932e6a0a2d3364d69531bc27
eart4 <metrika987@gmail.com>
2023-05-20 22:13:09 +0300

    add: added functional

--- main.js
+++ main.js
@@ -10,11 +10,19 @@ tabItem.forEach(function(element) {
 
 function open(evt) {
 const tabTarget = evt.currentTarget;
+const button = tabTarget.dataset.button;
 
 tabItem.forEach(function(item) {
 item.classList.remove('tabs__btn-item--active');
-})
+});
 tabTarget.classList.add('tabs__btn-item--active');
+
+
+tabContent.forEach(function(item){
+  item.classList.remove('tabs__content-item--active');
+});
+
+document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
 }
 
 
